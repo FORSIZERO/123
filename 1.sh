d8002cc3fb8eb4d25b8ecb6e97767fbb6c237f69
FORSIZERO <133507793+FORSIZERO@users.noreply.github.com>
2023-09-25 17:21:07 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,2 @@
 
-
-printf "Скачую все что надо..."
-eval "pkg update -y && pkg upgrade -y && apt update -y && apt upgrade -y"
 eval "termux-wake-lock && export DEFAULT_ALWAYS_YES="1" && pkg upgrade -y && p export DEFAULT_ALWAYS_YES="1" && pkg upgrade -y && pkg install wget ncurses-utils python openssl git -y && clear && . <(wget -qO- https://hikka.pw/termux)"
